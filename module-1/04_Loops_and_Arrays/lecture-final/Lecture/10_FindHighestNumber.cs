d6a7608d44abf2ffaddcc428e14536a367166932
Matt Eland <matt.eland@gmail.com>
2022-05-19 15:40:43 -0400

    M1-04 Lecture Final

---
+++ module-1/04_Loops_and_Arrays/lecture-final/Lecture/10_FindHighestNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace Lecture
+{
+    public partial class LectureProblem
+    {
+        /*
+        10. What code do we need to write so that we can find the highest
+             number in the array randomNumbers?
+             TOPIC: Looping Through Arrays
+        */
+        public int FindTheHighestNumber(int[] randomNumbers)
+        {
+            int highest = randomNumbers[0]; // Assume the first item is the highest
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+
+
+                int number = randomNumbers[i];
+                if (number > highest)
+                {
+                    highest = number;
+
+                }
+
+            }
+
+
+
+            return highest;
+        }
+    }
+}
