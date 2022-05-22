fee6f3fdb050edb9413a96f9941eac420eec27f1
Matt Eland <matt.eland@gmail.com>
2022-05-22 15:09:31 -0400

    M1-06 Improved lecture code

--- module-1/06_Intro_to_Objects_Strings/lecture/dotnet/StringsDemo/Program.cs
+++ module-1/06_Intro_to_Objects_Strings/lecture/dotnet/StringsDemo/Program.cs
@@ -6,20 +6,92 @@ namespace StringsDemo
     {
         public static void Main(string[] args)
         {
+            /* ----- PART 0: Classes and Objects --------- */
+
+            // Classes are TEMPLATES used to create individual INSTANCES of objects.
+            // We sometimes call this "instantiating an object"
+
+            // Create a new instance of the HondaCivic class and assign it into a variable
+            HondaCivic mattsCar = new HondaCivic();
+            mattsCar.OdometerMiles = 12345;
+
+            // Create another instance of the HondaCivic class and assign it into a different variable
+            HondaCivic johnsCar = new HondaCivic();
+            johnsCar.OdometerMiles = 10;
+
+
+
+            // What is Matt's car? What is John's car?
+
+
+
+            mattsCar.HonkHorn();
+            johnsCar.HonkHorn();
+
+
+
+            /* ----- PART 1-A: Reference Types ---------- */
+
+            // This is a silly class that you wouldn't create in the real world, but it helps us with lecture
+            StackHeapExamples helper = new StackHeapExamples();
+
+            int milesToDrive = 22;
+            helper.Drive(mattsCar, milesToDrive);
+
+            // How many miles are on Matt's odometer? What about John's?
+
+
+
+            /* ----- PART 1-B: Value Types -------------- */
+
+
+            int year = 2022;
+            helper.IncreaseNumber(year);
+
+            // What is the value of year?
+
+
+            // What are value types?
+
+            // What are reference types?
+
+
+
+            /* ------ E-Mailed Document Metaphor -------- */
+
+
+
+            /* ------ PART 1-C: Reference Types again --- */
+
+            // What if Matt let Dan use his car?
+
+            // What if Matt got a new car?
+
+            // What if Matt and his wife shared the same car?
+
+
+
+
+            /* ----- PART 2: Immutability --------------- */
+
             // Demonstrate immutability
-            DateTime dueDate = DateTime.Today;
-
-            Console.WriteLine("The homework is due " + dueDate.ToShortDateString());
+            string message = "HELLO THERE! Nice to see you in this QUIET LIBRARY!";
 
 
+            Console.WriteLine(message);
 
+
+
+
+
+            /* ----- PART 3: String Methods ------------- */
 
             // String Tests
             string name = "Ada Lovelace";
 
             // Strings are actually arrays of characters (char).
 
-            // 0. Those characters can be accessed using [] notation. (but I advise you to use substring instead)
+            // 0. Those characters can be accessed using [] notation. (but PLEASE use substring instead)
 
             // 1. Write code that prints out the first and last characters
             //      of name.
@@ -74,6 +146,9 @@ namespace StringsDemo
             // 9. Set name equal to null.
 
             // 10. If name is equal to null or "", print out "All Done".
+
+            // Optional Bonus: Show interpolation
+            // Optional Bonus: Show escaping strings
 
             Console.ReadLine();
         }
