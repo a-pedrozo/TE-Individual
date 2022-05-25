901cac0969c55c1309fa69c933734c5ea5b08df5
Matt Eland <matt.eland@gmail.com>
2022-05-25 14:22:45 -0400

    M1-09 Early Lecture

---
+++ module-1/09_Classes_Encapsulation/lecture/EncapsulationLecture/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EncapsulationLecture
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("First we drive a car");
+            Console.WriteLine();
+
+            Console.WriteLine("Then we do other things");
+            Console.WriteLine();
+
+            Console.WriteLine("Hey Matt, don't forget to talk about static and overloading");
+            Console.WriteLine();
+        }
+    }
+}
