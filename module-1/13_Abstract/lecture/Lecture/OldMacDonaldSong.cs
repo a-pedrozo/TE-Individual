253c50f6a610286519934ba3225bedafa0fd425a
Matt Eland <matt.eland@gmail.com>
2022-06-01 14:56:59 -0400

    M1-13 Revised Lecture

--- module-1/13_Abstract/lecture/Lecture/OldMacDonaldSong.cs
+++ module-1/13_Abstract/lecture/Lecture/OldMacDonaldSong.cs
@@ -9,50 +9,43 @@ namespace Lecture
     {
         public void Sing()
         {
-            Console.WriteLine("Old MacDonald had a farm ee ay ee ay oh");
-
-            // Let's try singing about a Farm Animal
-            FarmAnimal animal = new Horse();
-            SingAbout(animal);
-
-            // Let's try a different animal
+            // Declare things that live on our farm
+            FarmAnimal mrEd = new Horse();
             Whale moby = new Whale();
-            moby.Swim();
-            SingAbout(moby);
-
-            // What about multiple animals?
-            Console.WriteLine("Singing about many animals in a loop");
-            List<FarmAnimal> farm = new List<FarmAnimal>();
-            farm.Add(animal); // My horse from line 15
-            farm.Add(moby); // My whale from line 19
-
             Chicken chicken = new Chicken();
-            farm.Add(chicken);
 
             Whale bob = new Whale();
             bob.EatsPuppets = false;
-            farm.Add(bob);
 
-            foreach (FarmAnimal critter in farm)
-            {
-                SingAbout(critter);
-            }
-
-            // What about tractors?
             Tractor tractor = new Tractor();
-            SingAbout(tractor);
+
+            // Put everything in our farm in a list
+            List<ISingable> farm = new List<ISingable>();
+            farm.Add(mrEd);
+            farm.Add(moby);
+            farm.Add(chicken);
+            farm.Add(bob);
+            farm.Add(tractor);
+
+            // Sing about our farm
+            Console.WriteLine("Old MacDonald had a farm ee ay ee ay oh");
+
+            foreach (ISingable thing in farm)
+            {
+                SingAbout(thing);
+            }
 
             // If we have time, talk about static
         }
 
         // Anything passed in to this method must implement ISingable
-        private void SingAbout(ISingable animal)
+        private void SingAbout(ISingable thing)
         {
             // We know ISingables have MakeSoundOnce, MakeSoundTwice and Names
-            string soundOnce = animal.MakeSoundOnce();
-            string soundTwice = animal.MakeSoundTwice();
+            string soundOnce = thing.MakeSoundOnce();
+            string soundTwice = thing.MakeSoundTwice();
 
-            Console.WriteLine($"And on his farm there was a {animal.Name} ee ay ee ay oh");
+            Console.WriteLine($"And on his farm there was a {thing.Name} ee ay ee ay oh");
             Console.WriteLine($"With a {soundTwice} here and a {soundTwice} there");
             Console.WriteLine($"Here a {soundOnce}, there a {soundOnce} everywhere a {soundTwice}");
             Console.WriteLine("Old Macdonald had a farm, ee ay ee ay oh");
