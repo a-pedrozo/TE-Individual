4f63bb61caf64000a067c6b7f11e4926ef12d4d0
Matt Eland <matt.eland@gmail.com>
2022-06-01 14:23:42 -0400

    M1-13 Lecture, Exercise, Tutorial

---
+++ module-1/13_Abstract/lecture/Lecture/Farming/Tractor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Tractor : ISingable
+    {
+        public string Name { get; } = "John Deere";
+
+        public string MakeSoundOnce()
+        {
+            return  "Yeee-haw!";
+        }
+
+        public string MakeSoundTwice()
+        {
+            return "VROOM VROOM!";
+        }
+    }
+}
---
+++ module-1/13_Abstract/lecture/Lecture/Farming/Whale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Whale : FarmAnimal
+    {
+        public Whale() : base("WHAAAAAAAAAALE")
+        {
+            this.EatsPuppets = true;
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Just keep swimming");
+        }
+
+        public override string MakeSoundOnce()
+        {
+            return "I AM POLLLLYMORPHIIIIIICC";
+        }
+
+        public bool EatsPuppets { get; set; } = true;
+
+        public override string ToString()
+        {
+            return "Whale (Eats puppets: " + EatsPuppets + ")";
+        }
+    }
+}
