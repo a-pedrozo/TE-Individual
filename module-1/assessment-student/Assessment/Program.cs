fb8bd5895478805738f7dd619ec9cd39c9eaed85
augustine pedrozo <gpedrozo1100@gmail.com>
2022-06-07 10:05:25 -0400

    amusement park assessment

--- module-1/assessment-student/Assessment/Program.cs
+++ module-1/assessment-student/Assessment/Program.cs
@@ -8,6 +8,32 @@ namespace Assessment
         {
             // create an object and call methods on it (manual testing) from this class.
             // You DO NOT need to prompt the user to type in any values
+
+            TicketPurchase classTickets = new TicketPurchase("John", 19);
+
+            classTickets.Surcharge(true, true);
+            decimal baseTotal = classTickets.BasePrice * classTickets.NumOfTickets;
+            Console.WriteLine(baseTotal);
+
+
+
+
+            //    public override string ToString()
+            //{
+            //    return $" TICKET {} - {baseTotal}";
+            //}
+
+
+
+
+
+
+
+
+
+
         }
+
+
     }
 }
---
+++ module-1/assessment-student/Assessment/TicketPurchase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment
+{
+    public class TicketPurchase
+    {
+        public string Name { get; }
+        public int NumOfTickets { get; }
+        
+        public decimal BasePrice
+        {
+            get
+            {
+                decimal basePrice = NumOfTickets * 59.99m;
+                return basePrice;
+            }
+        }
+
+        public TicketPurchase(string name, int numOfTickets)
+        {
+            this.Name = name;
+            this.NumOfTickets = numOfTickets;
+        }
+
+        public decimal Surcharge(bool earlyCheckIn,bool priorityRideAccess)
+        {
+            decimal earlyCheckinCharge = 10.00m;
+            decimal priorityRideAccessCharge = 50.00m;
+            if(earlyCheckIn == true && priorityRideAccess == true)
+            {
+                return BasePrice + earlyCheckinCharge + priorityRideAccessCharge;
+            }
+            else if(earlyCheckIn == true)
+            {
+                return BasePrice + earlyCheckinCharge;
+            }
+            else if(priorityRideAccess == true)
+            {
+                return BasePrice + priorityRideAccessCharge;
+            }
+            else 
+            return BasePrice;
+        }
+
+    }
+}
