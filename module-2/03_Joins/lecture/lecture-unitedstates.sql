d9ae46f6ff3ccba97f1a8ce756a3d74859cb9610
Matt Eland <matt.eland@gmail.com>
2022-06-14 13:17:42 -0400

    M2-3 Lecture Modifications

--- module-2/03_Joins/lecture/lecture-unitedstates.sql
+++ module-2/03_Joins/lecture/lecture-unitedstates.sql
@@ -46,13 +46,3 @@
 
 -- Modify the previous query to include a column that indicates whether the place is a city or state.
 
-
-
--- MovieDB
--- After running the script to set up the MovieDB database, make sure it is selected in SSMS and confirm it is working correctly by writing queries to retrieve...
-
--- The names of all the movie genres
-
-
--- The titles of all the Comedy movies
-
