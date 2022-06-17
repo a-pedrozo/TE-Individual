4f8ffed5c1b0e06753cd0c4cb520d54601e91c87
Matt Eland <matt.eland@gmail.com>
2022-06-17 15:46:42 -0400

    Module 1 Mini-Capstone

---
+++ module-1/module-1_Mini-Capstone/Capstone/Classes/CateringItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    /// <summary>
+    /// This represents a single catering item in your system
+    /// </summary>
+    /// <remarks>
+    /// This class MUST be abstract
+    /// This class MUST be inherited by at least 2 other classes
+    /// Those other classes MUST be used in your program.
+    /// </remarks>
+    public abstract class CateringItem
+    {
+
+    }
+}
---
+++ module-1/module-1_Mini-Capstone/Capstone/Classes/FileAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    /// <summary>
+    /// This class should contain any and all details of access to files
+    /// </summary>
+    public class FileAccess
+    {
+        // All external data files for this application should live in this directory.
+        // You will likely need to create this directory and copy / paste any needed files.
+        private const string DataDirectory = @"C:\Catering";
+
+        // These files should be read from / written to in the DataDirectory
+        private const string CateringFileName = @"cateringsystem.csv";
+        private const string ReportFileName = @"totalsales.txt";
+    }
+}
