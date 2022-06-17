636e1ec3738c4701838b8a32146364a381a79032
augustine pedrozo <gpedrozo1100@gmail.com>
2022-06-17 16:47:17 -0400

    pushing SQL hw 4

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
@@ -1,6 +1,6 @@
 -- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet. Add yourself to the person table, and assign yourself as the director of "The Blob" (the movie is already in the movie table). (1 record each)
 --BEGIN TRANSACTION 
-SELECT * FROM movie WHERE title = 'The Blob'
+SELECT * FROM person WHERE person_name = 'Augustine Pedrozo'
 --367220
 
 INSERT INTO person(person_name, birthday)
@@ -10,6 +10,6 @@ VALUES ('Augustine Pedrozo', '19941209')
 --3984922
  
 UPDATE movie 
-SET director_id = 3984922
+SET director_id = 3984916
 WHERE movie_id = 367220
 --ROLLBACK TRANSACTION 
