b7c53cc177b9cf90b11f57b8810d7b3228d075e3
Matt Eland <matt.eland@gmail.com>
2022-06-23 14:48:31 -0400

    M2-10 Lecture Prep

---
+++ module-2/10_SQL_DAO_Review2/lecture/DadabaseApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace BugTrackerConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                string connectionString = LoadConnectionString();
+                // TODO: We'll need to do something with this connection string
+
+                // Create and run the user interface
+                UserInterface ui = new UserInterface();
+                ui.ShowMainMenu();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"Database error: {ex.Message}");
+            }
+        }
+
+        // Static methods are still bad, 'mmmkay? 
+        private static string LoadConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = config.GetConnectionString("DadJokes");
+
+            return connectionString;
+        }
+    }
+}
