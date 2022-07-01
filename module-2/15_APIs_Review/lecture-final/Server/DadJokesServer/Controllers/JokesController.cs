4d4ad4bcc2c1c94c4ff62185dc4558cd140db234
Matt Eland <matt.eland@gmail.com>
2022-07-01 14:32:12 -0400

    M2-15 Lecture Final

---
+++ module-2/15_APIs_Review/lecture-final/Server/DadJokesServer/Controllers/JokesController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DadJokesServer.DAOs;
+using DadJokesServer.Models;
+
+namespace DadJokesServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+        private readonly IJokeDAO jokeDAO;
+
+        public JokesController(IJokeDAO jokeDAO)
+        {
+            this.jokeDAO = jokeDAO;
+        }
+
+        [HttpGet] // GET Jokes
+        public ActionResult GetAllJokes()
+        {
+            List<Joke> jokes = jokeDAO.GetAllDadJokes();
+
+            return Ok(jokes);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetJoke(int id)
+        {
+            Joke joke = jokeDAO.GetJokeById(id);
+
+            if (joke == null)
+            {
+                return NotFound("Could not find joke " + id);
+            }
+
+            return Ok(joke);
+        }
+
+        [HttpPost]
+        public ActionResult CreateJoke(Joke joke)
+        {
+            Joke createdJoke = jokeDAO.AddJoke(joke);
+
+            return Ok(createdJoke); // or Created("jokes/" + createdJoke.Id, createdJoke);
+        }
+
+        [HttpPut("{jokeId}")]
+        public ActionResult UpdateJoke(int jokeId, [FromBody] Joke joke)
+        {
+            if (jokeId != joke.Id)
+            {
+                return BadRequest("The joke ID did not match. Nice try hacker!");
+            }
+
+            Joke updated = jokeDAO.UpdateJoke(joke);
+
+            if (updated == null)
+            {
+                return NotFound("Could not find joke " + jokeId);
+            }
+
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteAJoke(int id)
+        {
+            bool deleted = jokeDAO.DeleteJoke(id);
+
+            if (deleted)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ module-2/15_APIs_Review/lecture-final/Server/DadJokesServer/Models/Joke.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DadJokesServer.Models
+{
+    public class Joke
+    {
+        [Required]
+        [StringLength(255)]
+        public string Setup { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Punchline { get; set; }
+
+        public int Id { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public string DadName { get; set; }
+
+        public int? DadId { get; set; }
+    }
+}
