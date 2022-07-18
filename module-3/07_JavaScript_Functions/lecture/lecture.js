894acbcd50ead90660e8e6f4f62e34ad3f807617
Matt Eland <matt.eland@gmail.com>
2022-07-18 14:41:56 -0400

    M3-7 Lecture code

---
+++ module-3/07_JavaScript_Functions/lecture/lecture.js
@@ -0,0 +1,162 @@
+/**
+ * All named functions will have the function keyword and
+ * a name followed by parentheses.
+ * 
+ * @returns {number} 1
+ */
+function returnOne() {
+  return 1;
+}
+
+/**
+ * Functions can also take parameters. These are just variables that are filled
+ * in by whoever is calling the function.
+ *
+ * Also, we don't *have* to return anything from the actual function.
+ *
+ * @param {any} value the value to print to the console
+ */
+function printToConsole(value) {
+  console.log(value);
+}
+
+/**
+ * Takes an array and returns a new array of only numbers that are
+ * multiples of 3
+ *
+ * @param {number[]} numbersToFilter numbers to filter through
+ * @returns {number[]} a new array with only those numbers that are
+ *   multiples of 3
+ */
+function allDivisibleByThree(numbersToFilter) {
+
+}
+
+
+/**
+ * Functions can return earlier before the end of the function. This could be useful
+ * in circumstances where you may not need to perform additional instructions or have to
+ * handle a particular situation.
+ * In this example, if the firstParameter is equal to 0, we return secondParameter times 2.
+ * Observe what's printed to the console in both situations.
+ * 
+ * @param {number} firstParameter the first parameter
+ * @param {number} secondParameter the second parameter
+ */
+function returnBeforeEnd(firstParameter, secondParameter) {
+  console.log("This will always fire.");
+
+  if (firstParameter == 0) {
+    console.log("Returning secondParameter times two.");
+    return secondParameter * 2;
+  }
+
+  //this only runs if firstParameter is NOT 0
+  console.log("Returning firstParameter + secondParameter.");
+  return firstParameter + secondParameter;
+}
+
+/**
+ * Scope is defined as where a variable is available to be used.
+ *
+ * If a variable is declare inside of a block, it will only exist in
+ * that block and any block underneath it. Once the block that the
+ * variable was defined in ends, the variable disappears.
+ */
+function scopeTest() {
+  // This variable will always be in scope in this function
+  let inScopeInScopeTest = true;
+
+  {
+    // this variable lives inside this block and doesn't
+    // exist outside of the block
+    let scopedToBlock = inScopeInScopeTest;
+  }
+
+  // scopedToBlock doesn't exist here so an error will be thrown
+  if (inScopeInScopeTest && scopedToBlock) {
+    console.log("This won't print!");
+  }
+}
+
+/**
+ * Create an addThings method that takes in any number of number parameters and returns their total
+ */
+
+
+
+
+/**
+ * Write a function called multiplyTogether that multiplies two numbers together. But 
+ * what happens if we don't pass a value in? What happens if the value is not a number?
+ *
+ * @param {number} firstParameter the first parameter to multiply
+ * @param {number} secondParameter the second parameter to multiply
+ */
+
+
+
+
+/**
+ * This version makes sure that no parameters are ever missing. If
+ * someone calls this function without parameters, we default the
+ * values to 0. However, it is impossible in JavaScript to prevent
+ * someone from calling this function with data that is not a number.
+ * Call this function multiplyNoUndefined
+ *
+ * @param {number} [firstParameter=0] the first parameter to multiply
+ * @param {number} [secondParameter=0] the second parameter to multiply
+ */
+
+
+
+
+ // Let's document this function
+
+function createSentenceFromUser(name, age, listOfQuirks = [], separator = ', ') {
+  let description = `${name} is currently ${age} years old. Their quirks are: `;
+  return description + listOfQuirks.join(separator);
+}
+
+// Create a multiply function and store it in a variable
+
+// Create an anonymous multiply function and store it in a variable
+ 
+// Create an arrow multiply function and store it in a variable
+
+// Create a single line arrow multiply function and store it in a variable
+
+
+
+
+// Create an array of movies
+
+
+
+// Find - Find a specific movie
+
+// FindIndex - Find a movie by its index
+
+// Filter - Only get movies of a certain rating
+
+// Map - Select all movie titles
+
+// Reduce - Pull together the total runtime of all movies
+
+
+
+
+
+/**
+ * Takes an array and, using the power of anonymous functions, generates
+ * their sum.
+ *
+ * @param {number[]} numbersToSum numbers to add up
+ * @returns {number} sum of all the numbers
+ */
+function sumAllNumbers(numbersToSum) {
+  
+}
+
+// Create a function that gets movies that match a specific function
+
