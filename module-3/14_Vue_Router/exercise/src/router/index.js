d39f2401d049ae9290b76368d1d3f92eac769763
augustine pedrozo <gpedrozo1100@gmail.com>
2022-08-01 22:18:41 -0400

    pushing router hw

--- module-3/14_Vue_Router/exercise/src/router/index.js
+++ module-3/14_Vue_Router/exercise/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home';
+import MyBooks from '@/views/MyBooks';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/myBooks',
+    name: 'MyBooks',
+    component: MyBooks
   }
 ];
 
