4fff5b205d0a69aa0e1b95133cc1adef389d143d
John Fulton <john@techelevator.com>
2022-08-04 14:26:25 -0400

    Johns changes to final review code

---
+++ module-3/17_Review/lectureWithJohnsChanges/vue/src/views/UserList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>User List</h1>
+
+    <table>
+      <tr>
+        <th> Id </th>
+        <th> Username </th>
+        <th> Role </th>
+      </tr>
+      <tr v-for="item in user" v-bind:key="item.id">
+        <td>{{ item.userId }}</td>
+        <td>{{ item.username }}</td>
+        <td>{{ item.role }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import authService from "../services/AuthService";
+
+export default {
+  data() {
+    return {
+      user: [],
+    };
+  },
+
+  created() {
+    authService.getUsers().then((response) => {
+      this.user = response.data;
+      console.log(this.user);
+    });
+  },
+};
+</script>
+
+<style>
+</style>
